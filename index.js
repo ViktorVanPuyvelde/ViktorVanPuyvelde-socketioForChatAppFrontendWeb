ad47c99e2e791bcbca8e711146dd2c12ea88dd16
Viktor Van Puyvelde <viktor.vanpuyvelde2@student.hogent.be>
2021-11-29 21:52:53 +0100

    implementation of connection of a user

--- index.js
+++ index.js
@@ -4,6 +4,34 @@ const io = require("socket.io")(8900, {
   },
 });
 
+let users = [];
+
+const addUser = (userId, socketId) => {
+  !users.some(user => user.userId === userId) && users.push({
+    userId,
+    socketId
+  })
+}
+
+const removeUser = (socketId) => {
+  users = users.filter(u => u.socketId !== socketId);
+}
+
 io.on("connection", (socket) => {
-  
+  // when connect
+  console.log("a user connected")
+  // take userId and socketId from user
+  socket.on("addUser", userId => {
+    addUser(userId, socket.id);
+    io.emit("getUsers", users)
+  })
+
+  //send and get message
+
+  // when disconnect
+  socket.on("disconnect", () => {
+    console.log("a user disconnected!")
+    removeUser(socket.id)
+    io.emit("getUsers", users)
+  })
 })
